bc126a7bdb3967f5c270945374e9928068aa48a2
kimdaeun12 <1004ekdms2@naver.com>
2023-03-29 12:11:14 +0900

    1단계 완료

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+
+    public App (Scanner sc){
+        this.sc = sc;
+
+    }
+    public void run(){
+        System.out.println("==명언엡==");
+        while (true){
+            String command = sc.nextLine().trim();
+
+            if (command.equals("종료")){
+                break;
+            } else if (command.equals("등록")){
+                System.out.println(" 명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.println(" 작가 : ");
+                String content = sc.nextLine().trim();
+
+            }
+        }
+
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+import java.util.Scanner;
+public class Main {
+    Scanner sc = new Scanner(System.in);
+    new app(sc).run();
+    sc.close;
+}
